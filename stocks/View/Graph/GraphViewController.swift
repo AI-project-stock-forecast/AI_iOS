73b54d89e5695237a7bc0361f648e9d193051641
Taeh83 <dreamsky83@naver.com>
2022-05-20 09:50:58 +0900

    add search

--- stocks/View/Graph/GraphViewController.swift
+++ stocks/View/Graph/GraphViewController.swift
@@ -11,73 +11,142 @@ import Charts
 class GraphViewController: UIViewController {
     
     @IBOutlet weak var onepast : BarChartView?
-    @IBOutlet weak var nowpresent : BarChartView?
-    @IBOutlet weak var onefuture : BarChartView?
+    @IBOutlet weak var nowpresent : LineChartView?
+    @IBOutlet weak var onefuture : LineChartView?
     
-    var months : [String]!
-    var percent : [Double]!
-
+    @IBOutlet weak var company: UILabel? //회사
+    @IBOutlet weak var today: UILabel? //현재가
+    @IBOutlet weak var predict: UILabel? //예측률
+    
+    //값을 서버에서 어떤식으로 받을진 잘 모르겠다..
+    var times : [String]! //시각
+    var pastValue : [Double]! //값 ( 1주 전 )
+    var presentValue : [Double]! //값 ( 현재 )
+    var futureValue : [Double]! //값 ( 1주 후 )
+    
+    var dummy = ["애플", "삼성", "테슬라", "현대", "대한항공", "제주여객", "애플", "삼성", "테슬라", "현대", "대한항공", "제주여객", "애플", "삼성", "테슬라", "현대", "대한항공", "제주여객"]
+    
     @IBAction func switchGraph(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             onepast?.alpha = 1.0
             nowpresent?.alpha = 0.0
             onefuture?.alpha = 0.0
+            setPast(dates: times, values: pastValue)
         } else if sender.selectedSegmentIndex == 1 {
             onepast?.alpha = 0.0
             nowpresent?.alpha = 1.0
             onefuture?.alpha = 0.0
+            setPresent(dates: times, values: presentValue)
         } else {
             onepast?.alpha = 0.0
             nowpresent?.alpha = 0.0
             onefuture?.alpha = 1.0
+            setFuture(dates: times, nowValue: futureValue, predictValue: pastValue)
         }
     }
     
-    func setChart(dataPoints : [String], values : [Double]) {
+    func setPast(dates : [String], values : [Double]) {
         // 데이터 생성
         var dataEntries: [BarChartDataEntry] = []
-        for i in 0..<dataPoints.count {
+        for i in 0..<dates.count {
             let dataEntry = BarChartDataEntry(x: Double(i), y: values[i])
             dataEntries.append(dataEntry)
         }
 
         let chartDataSet = BarChartDataSet(entries: dataEntries, label: "판매량")
-        let stockDataSet = LineChartDataSet(entries: dataEntries, label: "상장")
 
         // 차트 컬러
         chartDataSet.colors = [.systemBlue]
-        stockDataSet.colors = [.systemRed]
 
         // 데이터 삽입
         let chartData = BarChartData(dataSet: chartDataSet)
         onepast?.data = chartData
         
+        // 선택 안되게
+        chartDataSet.highlightEnabled = false
+        
+        // 줌 안되게
+        onepast?.doubleTapToZoomEnabled = false
+        
+        // X축 레이블 위치 조정
+        onepast?.xAxis.labelPosition = .bottom
+        // X축 레이블 포맷 지정
+        onepast?.xAxis.valueFormatter = IndexAxisValueFormatter(values: dates)
+
+        // X축 레이블 갯수 최대로 설정 (이 코드 안쓸 시 Jan Mar May 이런식으로 띄엄띄엄 조금만 나옴)
+        onepast?.xAxis.setLabelCount(dates.count, force: false)
+        // 오른쪽 레이블 제거
+        onepast?.rightAxis.enabled = false
+    }
+    
+    func setPresent(dates : [String], values : [Double]) {
+        // 데이터 생성
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0..<dates.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let stockDataSet = LineChartDataSet(entries: dataEntries, label: "상장")
+        stockDataSet.colors = [.systemRed]
+        
+        stockDataSet.lineWidth = 2.0
+        stockDataSet.circleRadius = 2.0
+        stockDataSet.mode = .cubicBezier
+
         let stockData = LineChartData(dataSet: stockDataSet)
         nowpresent?.data = stockData
         
-        // 선택 안되게
-        chartDataSet.highlightEnabled = false
         stockDataSet.highlightEnabled = false
-        // 줌 안되게
-        onepast?.doubleTapToZoomEnabled = false
+        nowpresent?.xAxis.labelPosition = .bottom
+        nowpresent?.xAxis.valueFormatter = IndexAxisValueFormatter(values: dates)
         nowpresent?.doubleTapToZoomEnabled = false
-        onefuture?.doubleTapToZoomEnabled = false
-
-        // X축 레이블 갯수 최대로 설정 (이 코드 안쓸 시 Jan Mar May 이런식으로 띄엄띄엄 조금만 나옴)
-        onepast?.xAxis.setLabelCount(dataPoints.count, force: false)
-        nowpresent?.xAxis.setLabelCount(dataPoints.count, force: false)
-        // 오른쪽 레이블 제거
-        onepast?.rightAxis.enabled = false
+        nowpresent?.xAxis.setLabelCount(dates.count, force: false)
         nowpresent?.rightAxis.enabled = false
+    }
+    
+    func setFuture(dates : [String], nowValue : [Double], predictValue : [Double]) {
+        
+        // 데이터 생성
+        var nowEntries: [BarChartDataEntry] = []
+        //var predictEntries : [BarChartDataEntry] = []
+        
+        for i in 0..<dates.count {
+            let nowEntry = BarChartDataEntry(x: Double(i), y: nowValue[i])
+            nowEntries.append(nowEntry)
+        }
+        
+        let nowDataSet = LineChartDataSet(entries: nowEntries, label: "현재")
+        nowDataSet.colors = [.systemRed]
+        
+        nowDataSet.lineWidth = 2.0
+        nowDataSet.circleRadius = 2.0
+        nowDataSet.mode = .cubicBezier
+        
+        let nowData = LineChartData(dataSet: nowDataSet)
+        onefuture?.data = nowData
+        
+        nowDataSet.highlightEnabled = false
+        
+        onefuture?.xAxis.labelPosition = .bottom
+        onefuture?.xAxis.valueFormatter = IndexAxisValueFormatter(values: dates)
+        onefuture?.doubleTapToZoomEnabled = false
+        onefuture?.xAxis.setLabelCount(dates.count, force: false)
+        onefuture?.rightAxis.enabled = false
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        percent = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        times = ["9:00", "10:00", "11:00", "12:00", "13:00"]
+        pastValue = [20.0, 4.0, 6.0, 3.0, 12.0]
+        presentValue = [30.0, 5.0, 10.0, 7.0, 20.0]
+        futureValue = [6.0, 3.0, 12.0, 16.0, 9.0]
+
+        setPast(dates: times, values: pastValue)
         
-        setChart(dataPoints: months, values: percent)
+        //서버에서 받을때는 [String]이 아니라 String이므로.. 에러날일 없을듯함.
+        //company?.text = dummy
     }
 
 }
--- stocks/View/StockList/ListsViewController.swift
+++ stocks/View/StockList/ListsViewController.swift
@@ -7,35 +7,67 @@
 
 import UIKit
 import SnapKit
+import Then
 
 class ListsViewController: UIViewController {
-
-    @IBOutlet weak var listSearch: UISearchBar?
-    @IBOutlet weak var listTable: UITableView?
     
-    var allStockData : [stocks] = [] //모든 데이터
-    var filterStockData : [stocks] = [] //필터링된 데이터
+    @IBOutlet weak var listTable: UITableView?
+    @IBOutlet weak var nav: UINavigationItem?
+    
+    let listSearch = UISearchController()
+    
+    var company : [stocks] = [] //모든 회사이름
+    var filterCompany : [stocks] = [] //필터링된 회사이름
+    
+    var dummy = ["애플", "삼성", "테슬라", "현대", "대한항공", "제주여객", "애플", "삼성", "테슬라", "현대", "대한항공", "제주여객", "애플", "삼성", "테슬라", "현대", "대한항공", "제주여객"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         listTable?.delegate = self
         listTable?.dataSource = self
+        
+        searches()
     }
     
+    func searches() {
+        
+        listSearch.searchBar.placeholder = "회사명을 입력해주세요"
+        
+        nav?.titleView = listSearch.searchBar
+        nav?.hidesSearchBarWhenScrolling = false
+        
+        listSearch.searchResultsUpdater = self
+        listSearch.obscuresBackgroundDuringPresentation = false
+        listSearch.hidesNavigationBarDuringPresentation = false
+    }
+}
+
+extension ListsViewController : UISearchResultsUpdating {
+    
+    func filteredContentForSearchText(_ searchText:String){
+        filterCompany = company.filter( { (stocks) -> Bool in
+            return stocks.name.lowercased().contains(searchText.lowercased())
+        })
+        listTable?.reloadData()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredContentForSearchText(listSearch.searchBar.text ?? "")
+    }
 }
 
 extension ListsViewController : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return dummy.count //return company.count ( 약 100개정도? )
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = listTable?.dequeueReusableCell(withIdentifier: "list", for: indexPath) as! ListTableViewCell
         
-        cell.company?.text = "AAPL"
+        cell.company?.text = self.dummy[indexPath.row]
         cell.present?.text = "현재가"
         cell.presentmoney?.text = "280,000"
         cell.predicts?.text = "+30%"
